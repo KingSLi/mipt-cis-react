8eb4c04f18a222036cc48bc541227b4718d9ba8b
Polyanskiy Maksim <polyanskiy-mn@yandex-team.ru>
2021-11-03 14:39:33 +0300

    add first part of project

---
+++ washing_timetable/src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+               О нас: <a href='https://github.com/KingSLi'>Полянский Максим</a>               
+            </div>
+        )
+    }
+}
---
+++ washing_timetable/src/components/MainPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Typography } from 'antd';
+
+export class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <Typography.Title >Добро пожаловать!</Typography.Title>
+                <Typography.Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor orci, vestibulum at eleifend id, lobortis non eros. Suspendisse potenti. Curabitur vitae dapibus nisl. Donec porta nibh vel auctor congue. Donec non velit vehicula dolor finibus vulputate. In nulla justo, vulputate eget mauris et, convallis fringilla ex. Suspendisse pulvinar mauris ac mauris semper congue.
+                </Typography.Paragraph>
+                <Typography.Paragraph>
+                Aliquam felis tellus, ultrices vel facilisis a, tristique nec diam. Vivamus id tempus metus. Nulla nec mi velit. Duis auctor massa ac aliquam volutpat. Proin fermentum vel purus quis consequat. Sed porta turpis ipsum, congue mattis elit pharetra ac. Quisque elit quam, sagittis molestie pulvinar at, mattis eget urna. Phasellus massa tortor, lobortis ut ante quis, hendrerit sagittis turpis. Sed vel purus blandit, ultrices dolor eget, lacinia dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras iaculis ullamcorper ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum metus arcu, ornare aliquet pharetra a, cursus bibendum augue. Phasellus imperdiet nisi id odio scelerisque, vel tincidunt tellus ullamcorper. Cras rutrum porttitor ipsum ut dictum.
+                </Typography.Paragraph>  
+                <Typography.Title level={2}>О Париже!</Typography.Title>
+                <Typography.Paragraph>
+                Aenean vestibulum odio ligula, et suscipit diam ultricies vitae. Duis ornare, justo id pellentesque vestibulum, leo arcu sollicitudin dolor, et pharetra metus sem vel turpis. Phasellus lacinia odio mi, gravida placerat purus tincidunt ut. Duis suscipit convallis fermentum. Donec condimentum diam libero, ut facilisis sapien luctus vel. Curabitur eget nibh ac lorem sodales ultrices. Morbi a feugiat mauris. Duis interdum metus at velit gravida, ac lobortis quam venenatis. Nam nec ullamcorper leo. Mauris faucibus vitae tortor ac lacinia. Fusce vitae leo vel eros tempus mollis nec a urna. Maecenas at urna condimentum, malesuada lectus non, accumsan lorem. Fusce eget massa mattis ex vestibulum ullamcorper vitae eget ligula. Praesent imperdiet efficitur condimentum. Morbi at turpis id lectus malesuada elementum vel at tellus. Quisque et orci vel ex ultricies finibus sed non odio.
+                </Typography.Paragraph>  
+                <Typography.Paragraph>
+                In vitae malesuada urna, sagittis aliquet sem. Maecenas mollis consequat lacus nec congue. Nunc sodales rutrum ligula, ut varius metus commodo ut. Cras mattis rutrum turpis, eget ornare nisi malesuada a. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque et vestibulum augue, eu blandit sem. Etiam faucibus dolor a urna ornare, non lacinia elit volutpat. Morbi sapien risus, tristique dictum consectetur eu, fringilla ac sem.
+                </Typography.Paragraph>  
+                <Typography.Paragraph>
+                Nullam euismod placerat pulvinar. Proin quis ipsum arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse potenti. Maecenas fringilla nisl nec purus gravida ultrices. Donec fringilla lectus vitae leo ornare, id facilisis nisl pellentesque. In fermentum mattis ex at pellentesque. Phasellus imperdiet egestas eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur non convallis metus.
+                </Typography.Paragraph>                  
+            </div>
+        )
+    }
+}
+
